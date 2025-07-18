f196c27697911a75826f4df1d06a30e2407a92af
marcelbbb17 <marcelbbb17@gmail.com>
2025-07-18 10:04:19 +0100

    Made some database deployment changes

--- backend/config.py
+++ backend/config.py
@@ -11,9 +11,10 @@ class Config:
     
     # Database configuration
     DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = int(os.getenv('DB_PORT', 3306))
     DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'DonquixoteDoflamingo88')
-    DB_NAME = os.getenv('DB_NAME', 'suggestify')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+    DB_NAME = os.getenv('DB_NAME', '')
 
 class DevelopmentConfig(Config):
     DEBUG = True
--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
 import mysql.connector
+from config import get_config
 
 def get_db_connection():
-  connection = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'DonquixoteDoflamingo88',
-    database = 'suggestify'
-  )
-  return connection
+    config = get_config()
+    connection = mysql.connector.connect(
+        host = config.DB_HOST,
+        port = config.DB_PORT,
+        user = config.DB_USER,
+        password = config.DB_PASSWORD,
+        database = config.DB_NAME
+    )
+    return connection
